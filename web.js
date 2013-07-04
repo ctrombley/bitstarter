3d7216cd7469ff64d4e1f74034495b95cf86ad62
Chris Trombley <ctrombley@gmail.com>
2013-07-04 19:15:20 +0000

    updating web.js

--- web.js
+++ web.js
@@ -4,12 +4,18 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(req, res) {
-    var readStream = fs.createReadStream('index.html');
+
+
+    fs.createReadStream('index.html');
     readStream.on('open', function () {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
         readStream.pipe(res);
     });
 
     readStream.on('error', function () {
+        res.writeHead(500);
         res.end(err);
     });
 });
